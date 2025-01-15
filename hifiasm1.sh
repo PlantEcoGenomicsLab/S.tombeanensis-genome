ea6296f9304cec53cd1a26460b01593be3489e29
Anna Pasinato <145551653+bioannap@users.noreply.github.com>
2025-01-15 11:24:33 +0100

    Update hifiasm1.sh

--- hifiasm1.sh
+++ hifiasm1.sh
@@ -7,3 +7,8 @@ hifiasm -o /data/fdalgrande_data/Stombeanensis/S_tomb.asm -t 32 /data/fdalgrande
 awk '/^S/{print ">"$2;print $3}' S_tomb.asm.bp.hap1.p_ctg.gfa > S_tomb.asm.bp.hap1.p_ctg.fa &   #haplotype1
 awk '/^S/{print ">"$2;print $3}' S_tomb.asm.bp.hap2.p_ctg.gfa > S_tomb.asm.bp.hap2.p_ctg.fa &  #haplotype2
 awk '/^S/{print ">"$2;print $3}' S_tomb.asm.bp.p_ctg.gfa > S_tomb.asm.bp.p_ctg.fa &  #primary assembly
+
+#quality control of the assmebly can be done with asseblathon (note it reuires perl 5.10.1)
+perlbrew use 5.10.1
+perl5.10.1 -I /data/fdalgrande_data/tools/ /data/fdalgrande_data/tools/assemblathon_stats.pl ../S_tomb.asm.bp.p_ctg.fa > S_tomb.asm.bp.p_ctg.assemlathon_stats.txt
+exit
