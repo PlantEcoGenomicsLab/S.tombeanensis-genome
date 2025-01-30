f4ccda49b19edd0a8c47f99da9e651df44ce16d7
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-01-30 17:22:04 +0100

    Update FastQC.sh

--- FastQC.sh
+++ FastQC.sh
@@ -5,6 +5,8 @@ conda install bioconda::fastqc
 #transform bam file in fastq, because FastQC only accepts this format
 bam2fastq /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads.bam -o /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads &
 
-#run FastQC
+#create a directory where the results of the FastQC analysis will be saved
 mkdir fastqc_rawreads
+
+#run FastQC and save the results in the new directory
 fastqc S_tomb.hifi_reads.fastq -o fastqc_rawreads &
