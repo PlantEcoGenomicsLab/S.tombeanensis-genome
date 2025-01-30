b7a5c4a3d23e0464553984c379d0316ec0c87a48
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-01-30 18:52:42 +0100

    Update blobtools.sh

--- blobtools.sh
+++ blobtools.sh
@@ -2,9 +2,22 @@
 
 conda activate test
 
+#haplotype1
 blobtools create --fasta /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap1.p_ctg.fa \
  --cov /data/fdalgrande_data/Stombeanensis/minimap2/aln_S_tomb.asm.bp.hap1.sorted.bam \
  --busco busco_full_table_Stomb_hap1.tsv \
  --hits /data/fdalgrande_data/Stombeanensis/diamond/S_tomb.asm.bp.hap1.diamond.out \
  --taxdump /data/fdalgrande_data/tools/taxdump \
 blobtools_Stomb_hap1 &
+
+#haplotype2
+blobtools create --fasta /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap2.p_ctg.fa \
+ --cov /data/fdalgrande_data/Stombeanensis/minimap2/aln_S_tomb.asm.bp.hap2.sorted.bam \
+ --busco busco_full_table_Stomb_hap2.tsv \
+ --hits /data/fdalgrande_data/Stombeanensis/diamond/S_tomb.asm.bp.hap2.diamond.out \
+ --taxdump /data/fdalgrande_data/tools/taxdump \
+blobtools_Stomb_hap2 &
+
+#primary
+blobtools create --fasta /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.p_ctg.fa \
+--cov /data/fdalgrande_data/Stombeanensis/minimap2/aln_S_tomb.asm.bp.primary.sorted.bam \
