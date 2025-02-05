f84f99db58f3f259e289354257d3c0a3d2a43f4f
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-02-05 10:37:33 +0100

    Update busco.sh

--- busco.sh
+++ busco.sh
@@ -8,6 +8,7 @@
 
 conda activate test
 
+#evaluate the completeness of the genome assembly using BUSCO with the Viridiplantae database
 #hap1
 busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap1.p_ctg.fa \
