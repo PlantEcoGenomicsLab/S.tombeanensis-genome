29cd43475c8871bdc2e64d119234a2c388a7ba16
Anna Pasinato <145551653+bioannap@users.noreply.github.com>
2025-02-25 15:50:21 +0100

    Create chloroplast_oatk.sh

---
+++ organelles/chloroplast_oatk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+conda activate oatk
+
+#download database eudicotyledons Taxonomy ID: 71240; Saxifragales Taxonomy ID: 41946
+#create database
+/data/fdalgrande_data/tools/OatkDB/oatkdb -t 8 --output saxifragales_chloro 41946 chloroplast &
+
+#run oatk
+oatk -k 1001 -c 70 -t 10 -p saxifragales_chloro.fam  -o Stomb_plastid_oatk /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads.fastq.gz &
