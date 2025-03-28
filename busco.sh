4b2f339cd80924637e0f138fb93daba26e68cda6
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-03-28 15:25:54 +0100

    Update busco.sh

--- busco.sh
+++ busco.sh
@@ -8,14 +8,17 @@
 
 conda activate test
 
-#evaluate the completeness of the genome assembly using BUSCO with the Viridiplantae database
+#evaluate the completeness of the genome assembly using BUSCO
+
+#viridiplantae_odb10 database
 #hap1
 busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap1.p_ctg.fa \
  -l viridiplantae_odb10 \
  -m genome \
  -o S_tomb.asm.bp.hap1_completeness &
-
+ 
+#viridiplantae_odb10 database
 #hap2
 busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap2.p_ctg.fa \
@@ -23,9 +26,18 @@ busco \
  -m genome \
  -o S_tomb.asm.bp.hap2_completeness & 
 
+#viridiplantae_odb10 database
 #primary
 busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.p_ctg.fa \
  -l viridiplantae_odb10 \
  -m genome \
  -o S_tomb.asm.bp.primary_completeness &
+
+#eudicots_odb10 database
+#primary
+busco \
+ -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.p_ctg.fa \
+ -l eudicots_odb10 \
+ -m genome \
+ -o S_tomb.asm.bp.primary_eudi_completeness &
