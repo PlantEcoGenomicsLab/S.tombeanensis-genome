ebd36e13ff48f4d6c7efbf4749743b3dda816807
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-03-28 15:34:22 +0100

    Update busco_filter.sh

--- busco_filter.sh
+++ busco_filter.sh
@@ -8,7 +8,9 @@
 
 conda activate test
 
-#evaluate the completeness of the genome assembly using BUSCO with the Viridiplantae database after filtering
+#evaluate the completeness of the genome assembly after removing all non-Streptophyta using BUSCO
+
+#viridiplantae_odb10 database
 #hap1
 busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap1.p_ctg.filtered.fa \
@@ -16,16 +18,26 @@ busco \
  -m genome \
  -o S_tomb.asm.bp.hap1.filtered_completeness &
 
+#viridiplantae_odb10 database
 #hap2
 busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.hap2.p_ctg.filtered.fa \
  -l viridiplantae_odb10 \
  -m genome \
  -o S_tomb.asm.bp.hap2.filtered_completeness &
-
- #primary
- busco \
+ 
+#viridiplantae_odb10 database
+#primary
+busco \
  -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.p_ctg.filtered.fa \
  -l viridiplantae_odb10 \
  -m genome \
  -o S_tomb.asm.bp.filtered_completeness &
+
+#eudicots_odb10 database
+#primary
+ busco \
+ -i /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.p_ctg.filtered.fa \
+ -l eudicots_odb10 \
+ -m genome \
+ -o S_tomb.asm.bp.filtered_eudi_completeness &
