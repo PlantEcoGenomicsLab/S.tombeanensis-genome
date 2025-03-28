702c912e5a515c8d5f260e50e78b1be908b0c23f
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-03-28 15:03:26 +0100

    Create minimap2_filter.sh

---
+++ minimap2_filter.sh
@@ -0,0 +1,15 @@
+#PRIMARY_FILTERED
+  #!/bin/bash --login
+#SBATCH --job-name=minimap2_AP
+#SBATCH --error %x-%j.err
+#SBATCH --output %x-%j.out
+#SBATCH --partition=cpu
+#SBATCH --mem=300G
+#SBATCH --cpus-per-task=20
+
+conda activate test
+
+#run minimap2 to align the HiFi reads against the filtered primary assembly
+
+minimap2 -t 8 -ax map-pb /data/fdalgrande_data/Stombeanensis/S_tomb.asm.bp.p_ctg.filtered.fa /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads.fastq.gz \
+| samtools sort -o aln_S_tomb.asm.bp.primary.filtered.bam | samtools index &
