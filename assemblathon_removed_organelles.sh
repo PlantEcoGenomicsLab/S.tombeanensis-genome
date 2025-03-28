732ac23d16192f5b0ea804645e3c012f5fb00d7c
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-03-28 15:46:56 +0100

    Create assemblathon_removed_organelles.sh

---
+++ assemblathon_removed_organelles.sh
@@ -0,0 +1,4 @@
+##assemblathon for assessing quality of the assembly primary.filtered.removed_organelles
+perlbrew use 5.10.1
+perl5.10.1 -I /data/fdalgrande_data/tools/ /data/fdalgrande_data/tools/assemblathon_stats.pl /data/fdalgrande_data/Stombeanensis/blast/S_tomb_removed_organelles_80.fa > S_tomb_removed_organelles_80.assemlathon_stats.txt
+exit
