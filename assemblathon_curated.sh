7bedd198f349412c081881b24446c4f6d890a62a
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-03-28 15:49:04 +0100

    Create assemblathon_curated.sh

---
+++ assemblathon_curated.sh
@@ -0,0 +1,4 @@
+##assemblathon for assessing quality of the assembly primary cured with purge haplotigs
+perlbrew use 5.10.1
+perl5.10.1 -I /data/fdalgrande_data/tools/ /data/fdalgrande_data/tools/assemblathon_stats.pl /data/fdalgrande_data/Stombeanensis/assemblies/S_tomb_removed_organelles.curated.fasta > S_tomb_removed_organelles.curated.assemlathon_stats.txt
+exit
