4cab67bbc514a9b43c8377ed96982d637e234bb1
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-04-03 14:50:44 +0200

    Update mitochondrion_oatk.sh

--- Organelle Removal/mitochondrion_oatk.sh
+++ Organelle Removal/mitochondrion_oatk.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-#eudicotyledons Taxonomy ID: 71240 or Saxifragales Taxonomy ID: 41946
+ #!/bin/bash
 
-conda activate oatk
-
-#create oatk database
-/data/fdalgrande_data/tools/OatkDB/oatkdb -t 8 -o eudicotiledons_mito 71240 mitochondrion &
-
+#create the database from eudicotiledons in NCBI, Taxonomy ID: 71240
+data/fdalgrande_data/tools/OatkDB/oatkdb -t 8 --output eudicotyledons_mito 71240 mitochondrion &#conda activate oatk
 #run oatk
-oatk -k 1001 -c 70 -t 10 -m eudicotiledons_mito.fam -o Stomb_mito_oatk /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads.fastq.gz &
+oatk -k 1001 -c 70 -t 10 -m eudicotyledons_mito.fam -o Stomb_mito_oatk /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads.fastq.gz &
