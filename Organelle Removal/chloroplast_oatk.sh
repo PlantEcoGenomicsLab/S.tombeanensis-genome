d89f2b283bfdf7db3629bea073dca02fd2f18dfb
ValeriaFiorito <fioritovaleria00@gmail.com>
2025-04-03 14:44:49 +0200

    Update chloroplast_oatk.sh

--- Organelle Removal/chloroplast_oatk.sh
+++ Organelle Removal/chloroplast_oatk.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-conda activate oatk
+ #!/bin/bash
 
-#download database eudicotyledons Taxonomy ID: 71240; Saxifragales Taxonomy ID: 41946
+#create the database from saxifragales in NCBI, Taxonomy ID: 41946
 #create database
 /data/fdalgrande_data/tools/OatkDB/oatkdb -t 8 --output saxifragales_chloro 41946 chloroplast &
 
+#conda activate oatk
 #run oatk
 oatk -k 1001 -c 70 -t 10 -p saxifragales_chloro.fam  -o Stomb_plastid_oatk /data/fdalgrande_data/Stombeanensis/S_tomb.hifi_reads.fastq.gz &
